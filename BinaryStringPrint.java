819261ebdfbf33ed8286cc4e93f08e38af212b62
Bidyut Majhi <wdevlopment415@gmail.com>
2025-07-23 10:27:52 +0530

    add Binary Sting print consuqutive problem

---
+++ BinaryStringPrint.java
@@ -0,0 +1,19 @@
+public class BinaryStringPrint {
+
+  public static void printString(int n, int lastPlace,String str){
+    //base case 
+    if(n==0){
+      System.out.println(str);
+      return;
+    }
+    //recursion
+    printString(n-1, 0, str+"0");
+
+    if(lastPlace==0){
+      printString(n-1, 1, str+1);
+    }
+  }
+  public static void main(String[] args) {
+    printString(3, 0, "");
+  }
+}
